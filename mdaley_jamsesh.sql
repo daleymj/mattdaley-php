2798f238d95d379287210a0e3281f6ecb949cca8
daleymj <daleymj87@gmail.com>
2017-03-03 12:31:50 -0800

    database

--- mdaley_jamsesh.sql
+++ mdaley_jamsesh.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2017 at 08:25 PM
+-- Generation Time: Mar 03, 2017 at 09:30 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -47,7 +47,15 @@ DROP TABLE IF EXISTS `instruments`;
 CREATE TABLE IF NOT EXISTS `instruments` (
 `instrument_id` smallint(6) NOT NULL,
   `name` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `instruments`
+--
+
+INSERT INTO `instruments` (`instrument_id`, `name`) VALUES
+(1, 'drums'),
+(2, 'guitar');
 
 -- --------------------------------------------------------
 
@@ -77,7 +85,14 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `date` datetime NOT NULL,
   `user_id` smallint(6) NOT NULL,
   `photo` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`post_id`, `title`, `body`, `date`, `user_id`, `photo`) VALUES
+(1, 'Drums Rule!', 'They are the rhythm!', '2017-03-03 10:31:52', 1, '');
 
 -- --------------------------------------------------------
 
@@ -94,8 +109,19 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_pic` text NOT NULL,
   `date_joined` datetime NOT NULL,
   `account_type` tinyint(1) NOT NULL,
-  `is_admin` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `is_admin` tinyint(1) NOT NULL,
+  `zip_code` smallint(6) NOT NULL,
+  `security_key` varchar(40) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `user_pic`, `date_joined`, `account_type`, `is_admin`, `zip_code`, `security_key`) VALUES
+(1, 'daleymj', 'aece4b06c33d667a17af94fa22fe6cf7b83fceaa', 'daley@gmail.com', '', '2017-03-02 11:22:48', 1, 1, 32767, 'ae67c221ad705ac50d56f0ca7d74702c76ced34c'),
+(2, 'george', '59f566c930ad326955ed4adb5da7ff56d8bfa240', 'george@george.com', '', '2017-03-02 11:16:15', 3, 0, 32767, ''),
+(3, 'melissa', '59f566c930ad326955ed4adb5da7ff56d8bfa240', 'melissa@melissa.com', '', '2017-03-03 10:53:31', 3, 0, 32767, '5782b8aa25e6a077023b9eae6aa6e74ef67afa61');
 
 -- --------------------------------------------------------
 
@@ -163,7 +189,7 @@ MODIFY `comment_id` smallint(6) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `instruments`
 --
 ALTER TABLE `instruments`
-MODIFY `instrument_id` smallint(6) NOT NULL AUTO_INCREMENT;
+MODIFY `instrument_id` smallint(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `likes`
 --
@@ -173,12 +199,12 @@ MODIFY `like_id` smallint(6) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-MODIFY `post_id` smallint(6) NOT NULL AUTO_INCREMENT;
+MODIFY `post_id` smallint(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `user_id` smallint(6) NOT NULL AUTO_INCREMENT;
+MODIFY `user_id` smallint(6) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user_instruments`
 --
